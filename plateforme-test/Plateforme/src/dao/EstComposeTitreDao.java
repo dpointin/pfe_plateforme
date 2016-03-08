b304f18e921251ebe313d79bd0712a7e0265b9db
damien <damien.pointin@insa-rouen.fr>
2016-03-08 20:39:48 +0000

    mise en place dao

--- plateforme-test/Plateforme/src/dao/EstComposeTitreDao.java
+++ plateforme-test/Plateforme/src/dao/EstComposeTitreDao.java
@@ -1,5 +1,22 @@
 package dao;
 
+import java.util.Vector;
+
+import modele.Portefeuille;
+import modele.Titre;
+
 public interface EstComposeTitreDao {
 
+	//met a jour la quantite de l'obligation passé en entree dans le portefeuille 
+		//si elle n'est pas presente on la cree
+		//sinon s'il n'y en a plus on la supprime
+		//sinon juste un update
+		void mettreAJour(Portefeuille portefeuille, Titre titre) throws DAOException;
+		
+		// sert à supprimer lorsque le joueur supprime tout son portefeuille
+		void supprimer(Integer idPortefeuille) throws DAOException;
+		
+		
+		//Renvoie toutes les obligations correspondant au joueur
+		void trouver(Portefeuille portefeuille) throws DAOException;
 }
