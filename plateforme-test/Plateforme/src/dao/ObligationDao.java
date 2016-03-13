5a301bf84cb33ca4a9e2f0bd04a935b3c2b5e765
damien <damien.pointin@insa-rouen.fr>
2016-03-13 18:35:20 +0000

    obligationDao documente

--- plateforme-test/Plateforme/src/dao/ObligationDao.java
+++ plateforme-test/Plateforme/src/dao/ObligationDao.java
@@ -4,8 +4,38 @@ import java.util.Vector;
 
 import modele.Obligation;
 
+/**
+* Interface JoueurDao definissant les methodes que l'on peut realiser dans la table Obligation
+*
+* @author  Celine Chaugny & Damien Pointin 
+*/
 public interface ObligationDao {
-
+	/**
+	* Méthode chargée de recuperer les emetteurs de toutes les obligations
+	*
+	* @return Vector<String> correspondant a la liste des emetteurs d'obligation
+	* 
+	* @throws DAOException Si une erreur arrive lors de la lecture de la bdd
+	* 
+	* @see Obligation
+	* @see DAOException
+	* @see ObligationDaoImpl
+	*/ 
 	Vector<String> trouverToutesObligations() throws DAOException;
+	
+	
+	/**
+	* Méthode chargée de recuperer l'obligation qui correspond a un emetteur
+	* 
+	* @param emetteur de l'obligation
+	*
+	* @return Obligation correspondant a l'emetteur
+	* 
+	* @throws DAOException Si une erreur arrive lors de la lecture de la bdd
+	* 
+	* @see Obligation
+	* @see DAOException
+	* @see ObligationDaoImpl
+	*/
 	Obligation recupererObligation(String emetteur) throws DAOException;
 }
