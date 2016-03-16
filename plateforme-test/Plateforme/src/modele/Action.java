25eaedee307b57ec161bfa9230349d9a34ed4f3d
dpointin <damien.pointin@insa-rouen.fr>
2016-03-16 09:48:55 +0000

    documentation Action

--- plateforme-test/Plateforme/src/modele/Action.java
+++ plateforme-test/Plateforme/src/modele/Action.java
@@ -1,18 +1,58 @@
 package modele;
 
+/**
+* Classe Action representant une action du jeu 
+*
+* @author  Celine Chaugny & Damien Pointin 
+*/
 public class Action extends Titre {
-	
+	/**
+	* Le dividende de l'action.
+	*
+	* @see Action#Action(String, String, Integer, Double)
+	* @see Action#getDividende()
+	* @see Action#setDividende(Double)
+	*/ 
 	private Double dividende;
 
+	
+	/**
+	* Constructeur Action.
+	* <p>
+	* Avec les paramètres code, libelle, nombreDisponible et dividende
+	* correspondant aux entrees
+	* </p>
+	*
+	* @param code de l'action
+	* @param libelle de l'action
+	* @param nombreDisponible de l'action
+	* @param dividende de l'action
+	*
+	* @see Action#dividende
+	* @see Titre#Titre(String, String, Integer)
+	*/ 	
 	public Action(String code, String libelle, Integer nombreDisponible, Double dividende) {
 		super(code, libelle, nombreDisponible);
 		this.setDividende(dividende);
 	}
 
+	
+	/**
+	* Retourne le dividende de l'action.
+	*
+	* @return le dividende de l'action.
+	*/ 
 	public Double getDividende() {
 		return dividende;
 	}
 
+	
+	/**
+	* Met a jour le dividende de l'action.
+	*
+	* @param dividende , nouveau dividende de l'action.
+	*
+	*/ 
 	public void setDividende(Double dividende) {
 		this.dividende = dividende;
 	}
